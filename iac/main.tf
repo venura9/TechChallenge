533a1d422b6a1648092eb900a87c8a3a257dc1f7
Venura Athukorala <vathukorala@gmail.com>
2021-01-31 23:05:58 +1100

    added the basic components

--- iac/main.tf
+++ iac/main.tf
@@ -5,7 +5,6 @@ terraform {
       version = "~> 2.12"
     }
   }
-
   backend "remote" {}
 }
 
@@ -17,11 +16,52 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
-
 resource "azurerm_resource_group" "rg" {
   name     = "${var.app-name}-rg"
   location = var.app-location
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = replace("${var.app-name}-acr", "-", "")
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+
+# resource "null_resource" "azure-cli" {
+  
+#   provisioner "local-exec" {
+#     # azure cli script
+#     command = "run.sh"
+
+#     # terraform derived values and varialbes as env variables
+#     environment {
+#       webappname = "${azurerm_app_service.demo.name}"
+#       resourceGroup = ${azurerm_resource_group.demo.name}
+#     }
+#   }
+
+#   depends_on = [ azurerm_app_service.webapp ]
+# }
+
+# resource "azurerm_container_registry_webhook" "webhook" {
+#   name                = replace("${var.app-name}-webhook", "-", "")
+#   resource_group_name = azurerm_resource_group.rg.name
+#   registry_name       = azurerm_container_registry.acr.name
+#   location            = azurerm_resource_group.rg.location
+
+#   service_uri = "https://mywebhookreceiver.example/mytag"
+#   status      = "enabled"
+#   scope       = "${var.app-name}:latest"
+#   actions     = ["push"]
+#   custom_headers = {
+#     "Content-Type" = "application/json"
+#   }
+
+#   depends_on = [ azurerm_app_service.webapp ]
+# }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.app-name}-virtual-network"
@@ -50,6 +90,8 @@ resource "azurerm_app_service_plan" "asp" {
   name                = "${var.app-name}-asp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = "Standard"
@@ -62,9 +104,99 @@ resource "azurerm_app_service" "webapp" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
+  https_only          = true
+
+  site_config {
+    linux_fx_version = "DOCKER|nginx:latest"
+    health_check_path = "/healthcheck/"
+  }
+
+  app_settings = {
+    "WEBSITE_VNET_ROUTE_ALL"          = 1
+    "DOCKER_REGISTRY_SERVER_URL"      = azurerm_container_registry.acr.login_server
+    "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
+    "DOCKER_ENABLE_CI"                = "true"  
+  }
+
 }
 
+//scaling rule when processing hits 70%
+//two instances for high avaialbility (Note: There's already 99.95% uptime SLA backing an App Service)
+resource "azurerm_monitor_autoscale_setting" "cpuscale" {
+  name                = "AutoscaleSetting"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  target_resource_id  = azurerm_app_service_plan.asp.id
+
+  profile {
+    name = "default"
+
+    capacity {
+      default = 2
+      minimum = 2
+      maximum = 10
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_app_service_plan.asp.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 70
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_app_service_plan.asp.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 70
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+  }
+}
+
+// vnet integration with the integration subnet
 resource "azurerm_app_service_virtual_network_swift_connection" "vnet-intgr" {
   app_service_id = azurerm_app_service.webapp.id
   subnet_id      = azurerm_subnet.vnet-intgr-subnet.id
 }
+
+
+# data "external" "example" {
+#   # program = ["az", " webapp deployment container show-cd-url --name todo-app-webapp --resource-group todo-app-rg"]
+
+#   program = ["/bin/bash", "./run.sh"]
+
+#   # query = {
+#   #   # arbitrary map from strings to strings, passed
+#   #   # to the external program as the data query.
+#   #   id = "abc123"
+#   # }
+
+#   depends_on = [azurerm_app_service.webapp]
+# }
