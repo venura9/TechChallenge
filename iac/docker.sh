09c552701eeb694cb1461edb48534801dadeaf32
Venura Athukorala <vathukorala@gmail.com>
2021-02-04 10:41:34 +1100

    updated the one time setup a bit more

--- iac/docker.sh
+++ iac/docker.sh
@@ -11,9 +11,29 @@ DOCKER_PASSWORD=$(sed -e 's/^"//' -e 's/"$//' <<< $DOCKER_PASSWORD)
 
 DOCKER_IMAGE_SCOPE=$(terraform output docker_image_scope)
 DOCKER_IMAGE_SCOPE=$(sed -e 's/^"//' -e 's/"$//' <<< $DOCKER_IMAGE_SCOPE)
+
+DB_HOST=$(terraform output db_host)
+DB_HOST=$(sed -e 's/^"//' -e 's/"$//' <<< $DB_HOST)
+
+DB_USER=$(terraform output db_user)
+DB_USER=$(sed -e 's/^"//' -e 's/"$//' <<< $DB_USER)
+
+DB_PASSWORD=$(terraform output db_password)
+DB_PASSWORD=$(sed -e 's/^"//' -e 's/"$//' <<< $DB_PASSWORD)
+
+DB_NAME=$(terraform output db_name)
+DB_NAME=$(sed -e 's/^"//' -e 's/"$//' <<< $DB_NAME)
+
 cd ..
 
 # docker login https://${DOCKER_HOST}/v2/ -u $DOCKER_USER -p $DOCKER_PASSWORD 
 docker login $DOCKER_HOST -u $DOCKER_USER -p $DOCKER_PASSWORD 
 docker build . -t $DOCKER_IMAGE_SCOPE
 docker push ${DOCKER_HOST}/${DOCKER_IMAGE_SCOPE}
+
+export VTT_DBHOST=$DB_HOST
+export VTT_DBUSER=$DB_USER
+export VTT_DBPASSWORD=$DB_PASSWORD
+export VTT_DBNAME=$DB_NAME
+
+echo "docker run -it -e VTT_DBHOST -e VTT_DBUSER -e VTT_DBPASSWORD -e VTT_DBNAME ${DOCKER_IMAGE_SCOPE} updatedb -s"
--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,5 +1,5 @@
-output "docker_image_scope" {
-  value = replace("${var.app-name}/${var.environment}:latest", "-", "")
+output "docker_host" {
+  value = azurerm_container_registry.acr.login_server
 }
 
 output "docker_user" {
@@ -11,6 +11,23 @@ output "docker_password" {
   sensitive = true
 }
 
-output "docker_host" {
-  value = azurerm_container_registry.acr.login_server
+output "docker_image_scope" {
+  value = replace("${var.app-name}/${var.environment}:latest", "-", "")
+}
+
+output "db_host" {
+  value = "${azurerm_postgresql_server.pg_server.name}.postgres.database.azure.com"
+}
+
+output "db_user" {
+  value = "${var.db-user}@${azurerm_postgresql_server.pg_server.name}"
+}
+
+output "db_password" {
+  value = var.db-password
+  sensitive = true
+}
+
+output "db_name" {
+  value = azurerm_postgresql_database.pg_db.name
 }
